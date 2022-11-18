684828413428b3cc73af01b01805f9292699f172
Qahramon Uktamov <106351322+QahramonU@users.noreply.github.com>
2022-11-18 21:37:55 +0500

    project file has been uploaded

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+import pathlib
+temp=pathlib.PosixPath
+pathlib.PosixPath=pathlib.WindowsPath
+
+#Title
+st.title("CLASSIFIER OF TRANSPORTS")
+
+file=st.file_uploader("Upload file!", type=['png','jpeg', 'jpg','gif','svg'])
+model=load_learner("transports_model.pkl")
+if file:
+    st.image(file)
+    img=PILImage.create(file)
+    pred,pred_id, prob=model.predict(img)
+    st.success(f"PREDICTION: {pred}")
+    st.info(F"PROBABILITY: {prob[pred_id]*100:.1f}%")
+    fig=px.bar(x=prob*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
